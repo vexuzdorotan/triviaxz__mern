b4dd5024c76c2ced490d2c980a01cedb3136bbf3
VeXuZ <vexuzdorotan@gmail.com>
2020-08-07 20:33:27 +0800

    setHeaders

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+  });
+
+  next();
+});
+
 app.use('/api/users', userRouters);
 app.use('/api/scores', scoreRouters);
 
